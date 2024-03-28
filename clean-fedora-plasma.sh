7e621edf33f7c6951d6d4ac8731ffe2e4a0ea17b
Viet Huynh <xa.lach@yahoo.com>
2024-03-29 01:18:41 +0700

    clean more packages

--- clean-fedora-plasma.sh
+++ clean-fedora-plasma.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo dnf -y erase ark krdc firewalld kde-partitionmanager elisa-player dragon gwenview kcalc kfind kde-connect kf5-akonadi-server kmahjongg kmines kolourpaint konversation kpat krfb kwrite filelight mediawriter libreoffice-core libreoffice-common
+sudo dnf -y erase ark krdc firewalld kde-partitionmanager khelpcenter plasma-welcome elisa-player dragon kfind kde-connect kf5-akonadi-server kmahjongg kmines kolourpaint konversation kpat krfb kwrite kcalc kcharselect kmousetool filelight mediawriter qt5-qdbusviewer libreoffice-core libreoffice-common
 sudo dnf -y autoremove
