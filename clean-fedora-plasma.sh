a147141cf31c2e20bf3a575812d79605d72f31fb
Viet Huynh <xa.lach@yahoo.com>
2024-03-28 17:46:34 +0700

    clean more packages

--- clean-fedora-plasma.sh
+++ clean-fedora-plasma.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo dnf -y erase elisa-player dragon gwenview kcalc kde-connect kf5-akonadi-server kmahjongg kmines kolourpaint konversation kpat krfb kwrite filelight mediawriter libreoffice-core libreoffice-common
+sudo dnf -y erase ark krdc firewalld kde-partitionmanager elisa-player dragon gwenview kcalc kfind kde-connect kf5-akonadi-server kmahjongg kmines kolourpaint konversation kpat krfb kwrite filelight mediawriter libreoffice-core libreoffice-common
 sudo dnf -y autoremove
