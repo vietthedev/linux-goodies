218727d694829594398546382a4f764b197c0da5
Viet Huynh <xa.lach@yahoo.com>
2024-03-28 17:16:48 +0700

    add scripts to remove bloat

---
+++ clean-fedora-gnome.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo dnf -y erase libreoffice-core libreoffice-common
+sudo dnf -y autoremove
---
+++ clean-fedora-plasma.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo dnf -y erase elisa-player dragon gwenview kcalc kde-connect kf5-akonadi-server kmahjongg kmines kolourpaint konversation kpat krfb kwrite filelight mediawriter libreoffice-core libreoffice-common
+sudo dnf -y autoremove
