7d2b124f86067467ad71038c2cae36cb3d24791f
Viet Huynh <xa.lach@yahoo.com>
2024-03-28 18:06:21 +0700

    explicitly install dbus-glib

--- install-packages-fedora-plasma.sh
+++ install-packages-fedora-plasma.sh
@@ -17,4 +17,4 @@ sudo dnf install -y intel-media-driver
 sudo dnf install -y --allowerasing ffmpeg
 
 # Install essential packages
-sudo dnf install -y curl unzip htop neovim fcitx5-unikey fcitx5-autostart
+sudo dnf install -y curl unzip htop neovim fcitx5-unikey fcitx5-autostart dbus-glib
