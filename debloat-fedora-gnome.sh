b3d3065c623d8390034527bf5e2a74cbef17c331
Viet Huynh <xa.lach@yahoo.com>
2024-04-23 05:56:04 +0700

    update scripts

--- debloat-fedora-gnome.sh
+++ debloat-fedora-gnome.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo dnf -y erase libreoffice-core libreoffice-common
+sudo dnf -y erase libreoffice-core libreoffice-common gnome-contacts baobab simple-scan yelp gnome-maps rhythmbox totem gnome-boxes
 sudo dnf -y autoremove
---
+++ install-packages-fedora-gnome.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Install fish shell
+sudo dnf -y install fish
+
+# Install starship shell prompt
+sudo dnf -y copr enable atim/starship
+sudo dnf -y install starship
+
+# Install RPM Fusion repositories
+sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# Install graphics driver
+sudo dnf install -y intel-media-driver
+
+# Install full ffmpeg
+sudo dnf install -y --allowerasing ffmpeg
+
+# Install ibus-bamboo
+sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:lamlng/Fedora_39/home:lamlng.repo
+sudo dnf install -y ibus-bamboo
+
+# Install essential packages
+sudo dnf install -y curl unzip htop neovim gnome-tweaks gnome-extensions-app
