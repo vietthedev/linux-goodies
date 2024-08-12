f850c2bf36e846e06c3789c65d324d9ad7a9005d
Viet Huynh <xa.lach@yahoo.com>
2024-08-12 13:03:18 +0700

    update ibus-bamboo URL

--- install-packages-fedora-gnome.sh
+++ install-packages-fedora-gnome.sh
@@ -17,7 +17,7 @@ sudo dnf install -y intel-media-driver
 sudo dnf install -y --allowerasing ffmpeg
 
 # Install ibus-bamboo
-sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:lamlng/Fedora_39/home:lamlng.repo
+sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:lamlng/Fedora_40/home:lamlng.repo
 sudo dnf install -y ibus-bamboo
 
 # Install essential packages
