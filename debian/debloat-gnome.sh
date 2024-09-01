784bdc8b550d5ad6591d157ef40073067b134d57
Viet Huynh <xa.lach@yahoo.com>
2024-09-02 02:19:38 +0700

    remove gnome-music

--- debian/debloat-gnome.sh
+++ debian/debloat-gnome.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo apt -y autopurge evolution synaptic gnome-games gnome-sound-recorder libreoffice-core libreoffice-common gnome-contacts baobab simple-scan yelp gnome-maps rhythmbox totem transmission-gtk
+sudo apt -y autopurge evolution synaptic gnome-games gnome-sound-recorder gnome-music libreoffice-core libreoffice-common gnome-contacts baobab simple-scan yelp gnome-maps rhythmbox totem transmission-gtk
