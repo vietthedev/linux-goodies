b44c782b2c7fe24f124c5bcc4b7fa08234840d33
Viet Huynh <xa.lach@yahoo.com>
2024-09-04 11:15:59 +0700

    add Noto Sans fonts

--- debian/install-fonts.sh
+++ debian/install-fonts.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
-# Install FiraCode Nerd font
+# Install FiraCode Nerd Font
 curl -LJO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraCode.zip
 unzip FiraCode.zip -d tmp
 sudo mkdir -p /usr/share/fonts/truetype/firacode-nerd
 sudo mv tmp/*.ttf /usr/share/fonts/truetype/firacode-nerd/
 rm -rf tmp FiraCode.zip
 
-# Install Cascadia Code Nerd font
+# Install Cascadia Code Nerd Font
 curl -LJO https://github.com/microsoft/cascadia-code/releases/download/v2404.23/CascadiaCode-2404.23.zip
 unzip CascadiaCode-2404.23.zip -d tmp
 sudo mkdir -p /usr/share/fonts/truetype/cascadia-code-nerd
 sudo mv tmp/ttf/CascadiaCode*.ttf /usr/share/fonts/truetype/cascadia-code-nerd
 rm -rf tmp CascadiaCode-2404.23.zip
+
+# Install Noto Sans fonts
+sudo apt -y install fonts-noto
 
 # Install Microsoft fonts
 sudo apt -y install ttf-mscorefonts-installer
